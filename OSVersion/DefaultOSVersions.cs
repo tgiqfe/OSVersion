1a0e29d1b62e561b327898a46b85e57e396c0854
tgiqfe <tqji31001@gmail.com>
2019-10-29 14:22:05 +0900

    Init

---
+++ OSVersion/DefaultOSVersions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSVersion
+{
+    class DefaultOSVersions
+    {
+        public static OSVersion[] GetOSVersions()
+        {
+            return new OSVersion[]
+            {
+                new OSVersion()
+                {
+                    Version = OSVersion.v1507,
+                    Alias = "Released in July 2015",
+                    BuildNumber = "10240",
+                    FullVersion = "10.0.10240",
+                    ReleaseDate = DateTime.Parse("2015/07/29")
+                },
+                new OSVersion()
+                {
+                    Version = OSVersion.v1511,
+                    Alias = "November Update",
+                    BuildNumber = "10586",
+                    FullVersion = "10.0.10586",
+                    ReleaseDate = DateTime.Parse("2015/11/12")
+                },
+                new OSVersion()
+                {
+                    Version = OSVersion.v1607,
+                    Alias = "Anniversary Update",
+                    BuildNumber = "14393",
+                    FullVersion = "10.0.14393",
+                    ReleaseDate = DateTime.Parse("2016/08/02")
+                },
+                new OSVersion()
+                {
+                    Version = OSVersion.v1703,
+                    Alias = "Creators Update",
+                    BuildNumber = "15063",
+                    FullVersion = "10.0.15063",
+                    ReleaseDate = DateTime.Parse("2017/04/11")
+                },
+                new OSVersion()
+                {
+                    Version = OSVersion.v1709,
+                    Alias = "Fall Creators Update",
+                    BuildNumber = "16299",
+                    FullVersion = "10.0.16299",
+                    ReleaseDate = DateTime.Parse("2017/10/17")
+                },
+                new OSVersion()
+                {
+                    Version = OSVersion.v1803,
+                    Alias = "April 2018 Update",
+                    BuildNumber = "17134",
+                    FullVersion = "10.0.17134",
+                    ReleaseDate = DateTime.Parse("2018/04/30")
+                },
+                new OSVersion()
+                {
+                    Version = OSVersion.v1809,
+                    Alias = "October 2018 Update",
+                    BuildNumber = "17763",
+                    FullVersion = "10.0.17763",
+                    ReleaseDate = DateTime.Parse("2018/11/13")
+                },
+                new OSVersion()
+                {
+                    Version = OSVersion.v1903,
+                    Alias = "May 2019 Update",
+                    BuildNumber = "18362",
+                    FullVersion = "10.0.18362",
+                    ReleaseDate = DateTime.Parse("2019/05/21")
+                },
+                new OSVersion()
+                {
+                    Version = OSVersion.v1909,
+                    Alias = "November 2019 Update",
+                    BuildNumber = "-----",
+                    FullVersion = "10.0.-----",
+                    ReleaseDate = DateTime.Parse("2019/11/11")  //  ←まだ不明
+                },
+            };
+        }
+    }
+}
---
+++ OSVersion/OSVersion.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace OSVersion
+{
+    class OSVersion
+    {
+        public const int v1507 = 1507;
+        public const int v1511 = 1511;
+        public const int v1607 = 1607;
+        public const int v1703 = 1703;
+        public const int v1709 = 1709;
+        public const int v1803 = 1803;
+        public const int v1809 = 1809;
+        public const int v1903 = 1903;
+        public const int v1909 = 1909;
+
+        public string Name { get { return this.Version.ToString(); } }
+        public int Version { get; set; }
+        public string Alias { get; set; }
+        public string BuildNumber { get; set; }
+        public string FullVersion { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Edition { get; set; }
+
+        /// <summary>
+        /// パラメータ無しのコンストラクタ。とりあえず作るだけ。
+        /// </summary>
+        public OSVersion() { }
+
+        /// <summary>
+        /// int型のバージョン名から
+        /// </summary>
+        /// <param name="version"></param>
+        public OSVersion(int version)
+        {
+            switch (version)
+            {
+                case v1507:
+                    this.Version = v1507;
+                    this.Alias = "Released in July 2015";
+                    this.BuildNumber = "10240";
+                    this.FullVersion = "10.0.10240";
+                    this.ReleaseDate = DateTime.Parse("2015/07/29");
+                    break;
+                case v1511:
+                    this.Version = v1511;
+                    this.Alias = "November Update";
+                    this.BuildNumber = "10586";
+                    this.FullVersion = "10.0.10586";
+                    this.ReleaseDate = DateTime.Parse("2015/11/12");
+                    break;
+                case v1607:
+                    this.Version = v1607;
+                    this.Alias = "Anniversary Update";
+                    this.BuildNumber = "14393";
+                    this.FullVersion = "10.0.14393";
+                    this.ReleaseDate = DateTime.Parse("2016/08/02");
+                    break;
+                case v1703:
+                    this.Version = v1703;
+                    this.Alias = "Creators Update";
+                    this.BuildNumber = "15063";
+                    this.FullVersion = "10.0.15063";
+                    this.ReleaseDate = DateTime.Parse("2017/04/11");
+                    break;
+                case v1709:
+                    this.Version = v1709;
+                    this.Alias = "Fall Creators Update";
+                    this.BuildNumber = "16299";
+                    this.FullVersion = "10.0.16299";
+                    this.ReleaseDate = DateTime.Parse("2017/10/17");
+                    break;
+                case v1803:
+                    this.Version = v1803;
+                    this.Alias = "April 2018 Update";
+                    this.BuildNumber = "17134";
+                    this.FullVersion = "10.0.17134";
+                    this.ReleaseDate = DateTime.Parse("2018/04/30");
+                    break;
+                case v1809:
+                    this.Version = v1809;
+                    this.Alias = "October 2018 Update";
+                    this.BuildNumber = "17763";
+                    this.FullVersion = "10.0.17763";
+                    this.ReleaseDate = DateTime.Parse("2018/11/13");
+                    break;
+                case v1903:
+                    this.Version = v1903;
+                    this.Alias = "May 2019 Update";
+                    this.BuildNumber = "18362";
+                    this.FullVersion = "10.0.18362";
+                    this.ReleaseDate = DateTime.Parse("2019/05/21");
+                    break;
+                case v1909:
+                    Version = v1909;
+                    Alias = "November 2019 Update";
+                    BuildNumber = "-----";
+                    FullVersion = "10.0.-----";
+                    ReleaseDate = DateTime.Parse("2019/11/11");  //  ←まだ不明
+                    break;
+            }
+        }
+
+
+
+        public OSVersion(string fullVersion)
+        {
+
+        }
+
+
+        public static OSVersion GetOSVersion()
+        {
+            ManagementObject mo = new ManagementClass("Win32_OperatingSystem").
+                GetInstances().
+                OfType<ManagementObject>().
+                First();
+            OSVersion osver = DefaultOSVersions.GetOSVersions().FirstOrDefault(x => x.FullVersion == mo["Version"].ToString());
+            osver.Edition = mo["Caption"] as string;
+
+            return osver;
+        }
+
+        /// <summary>
+        /// 比較演算子 < 小なり
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator <(OSVersion x, OSVersion y) { return x.Version < y.Version; }
+        public static bool operator <(OSVersion x, int y) { return x.Version < y; }
+        public static bool operator <(int x, OSVersion y) { return x < y.Version; }
+
+        /// <summary>
+        /// 比較演算子 > 大なり
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator >(OSVersion x, OSVersion y) { return x.Version < y.Version; }
+        public static bool operator >(OSVersion x, int y) { return x.Version > y; }
+        public static bool operator >(int x, OSVersion y) { return x > y.Version; }
+
+        /// <summary>
+        /// 比較演算子 <= 小なりイコール
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator <=(OSVersion x, OSVersion y) { return x.Version <= y.Version; }
+        public static bool operator <=(OSVersion x, int y) { return x.Version <= y; }
+        public static bool operator <=(int x, OSVersion y) { return x <= y.Version; }
+
+        /// <summary>
+        /// 比較演算子 >= 大なりイコール
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator >=(OSVersion x, OSVersion y) { return x.Version >= y.Version; }
+        public static bool operator >=(OSVersion x, int y) { return x.Version >= y; }
+        public static bool operator >=(int x, OSVersion y) { return x >= y.Version; }
+
+        /// <summary>
+        /// 比較演算子 ==
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator ==(OSVersion x, OSVersion y) { return x.Version == y.Version; }
+
+        /// <summary>
+        /// 比較演算子 !=
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator !=(OSVersion x, OSVersion y) { return x.Version != y.Version; }
+
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj) { return this.Version == ((OSVersion)obj).Version; }
+
+        /// <summary>
+        /// GetHashCode
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode() { return base.GetHashCode(); }
+    }
+}
