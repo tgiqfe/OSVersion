2eb9d49f1753ba8cf1960026a2779ab273f0c038
tgiqfe <tqji31001@gmail.com>
2022-01-29 21:58:01 +0900

    Add OSBase class.

---
+++ OSVersion/OSVersion/Lib/OSBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSVersion.Lib
+{
+    internal class OSBase
+    {
+        #region Public parameter
+
+        /// <summary>
+        /// OSの名前
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// OS名のエイリアス
+        /// </summary>
+        public string[] Alias { get; set; }
+
+        /// <summary>
+        /// OS種類。Windows/Linux/Mac
+        /// ※今回のバージョンでは全部Windows
+        /// </summary>
+        public OSFamily OSFamily { get; set; }
+
+        /// <summary>
+        /// バージョン名称
+        /// </summary>
+        public string VersionName { get; set; }
+
+        /// <summary>
+        /// バージョン名のエイリアス
+        /// </summary>
+        public string[] VersionAlias { get; set; }
+
+        /// <summary>
+        /// サーバOSかどうか
+        /// </summary>
+        public bool? ServerOS { get; set; }
+
+        /// <summary>
+        /// 組み込みOSかどうか
+        /// </summary>
+        public bool? EmbeddedOS { get; set; }
+
+        #endregion
+
+        protected int Serial { get; }
+
+
+    }
+}
---
+++ OSVersion/OSVersion/Lib/OSCompare.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSVersion.Lib
+{
+    internal class OSCompare : OSBase
+    {
+        
+        //  ここでoperatorメソッドを
+
+    }
+}
