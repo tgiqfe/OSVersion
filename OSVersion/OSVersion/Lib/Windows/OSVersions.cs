d5bd8d16a75afeced86825f7ae2c8871b298a0a6
tgiqfe <tqji31001@gmail.com>
2022-02-16 00:43:45 +0900

    clean code.

--- OSVersion/OSVersion/Lib/Windows/OSVersions.cs
+++ OSVersion/OSVersion/Lib/Windows/OSVersions.cs
@@ -19,46 +19,37 @@ namespace OSVersion.Lib.Windows
             list.Add(new OSVersions()
             {
                 Name = "1507",
-                Candidate = new string[]
-                {
-                    "1507", "v1507", "10.0.10240", "Released in July 2015", "ReleasedinJuly2015", "Threshold 1", "Threshold1"
-                },
+                Candidate = new string[] { "1507", "v1507", "10.0.10240", "Released in July 2015", "ReleasedinJuly2015", "Threshold 1", "Threshold1" },
                 Description = "Windows 10のリリース時バージョン"
             });
             list.Add(new OSVersions()
             {
                 Name = "1511",
-                Candidate = new string[]
-                {
-                    "1511", "v1511"
-                },
+                Candidate = new string[] { "1511", "v1511" },
                 Description = "Windows 10のリリース後の次のバージョン"
             });
             list.Add(new OSVersions()
             {
                 Name = "1607",
-                Candidate = new string[]
-                {
-                    "1607", "v1607"
-                },
+                Candidate = new string[] { "1607", "v1607" },
                 Description = ""
             });
             list.Add(new OSVersions()
             {
                 Name = "1703",
-                Candidate = new string[]
-                {
-                    "1703", "v1703"
-                },
+                Candidate = new string[] { "1703", "v1703" },
                 Description = ""
             });
             list.Add(new OSVersions()
             {
                 Name = "1709",
-                Candidate = new string[]
-                {
-                    "1709", "v1709"
-                },
+                Candidate = new string[] { "1709", "v1709" },
+                Description = ""
+            });
+            list.Add(new OSVersions()
+            {
+                Name = "1803",
+                Candidate = new string[] { "1803", "v1803" },
                 Description = ""
             });
 
