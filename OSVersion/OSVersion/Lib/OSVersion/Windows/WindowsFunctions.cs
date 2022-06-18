304df568a50807c8d7a69c6d2358f72259659eb9
tgiqfe <tqji31001@gmail.com>
2022-06-18 23:30:16 +0900

    Add WindowsOSRange class.

--- OSVersion/OSVersion/Lib/OSVersion/Windows/WindowsFunctions.cs
+++ OSVersion/OSVersion/Lib/OSVersion/Windows/WindowsFunctions.cs
@@ -92,12 +92,45 @@ namespace OSVersion.Lib.OSVersion.Windows
         }
 
 
+        public class WindowsOSRange
+        {
+            public OSInfo[] Minimum { get; set; }
+            public OSInfo[] Maximum { get; set; }
+            public WindowsOSRange(OSCollection collection, string text)
+            {
+                string[] osWord = text.Contains("~") ?
+                    new[] { text.Substring(0, text.IndexOf("~")), text.Substring(text.IndexOf("~") + 1) } :
+                    new[] { text, text };
+                this.Minimum = string.IsNullOrEmpty(osWord[0]) ?
+                    new OSInfo[] { AnyOS.CreateMinimum() } :
+                    collection.Where(x => x.IsMatch(osWord[0])).ToArray();
+                this.Maximum = string.IsNullOrEmpty(osWord[1]) ?
+                    new OSInfo[] { AnyOS.CreateMaximum() } :
+                    collection.Where(x => x.IsMatch(osWord[1])).ToArray();
+            }
+            public bool Within(WindowsOS current)
+            {
+                bool minRet = Minimum.Where(x => x.OSFamily == OSFamily.Any || x.Name == current.Name).
+                    Any(x => x <= current);
+                bool maxRet = Maximum.Where(x => x.OSFamily == OSFamily.Any || x.Name == current.Name).
+                    Any(x => x >= current);
+
+                return minRet && maxRet;
+            }
+        }
+
+        public static bool WithinOS(OSCollection collection, WindowsOS current, string text)
+        {
+            var list = new List<WindowsOSRange>();
+            foreach (string field in text.Split(","))
+            {
+                list.Add(new WindowsOSRange(collection, field));
+            }
+
+        }
 
 
-
-
-
-        public static bool WithinOS(OSCollection collection, WindowsOS current, string targetOSText)
+        public static bool WithinOS2(OSCollection collection, WindowsOS current, string targetOSText)
         {
             string[] fields = targetOSText.Split(",");
             foreach (string field in fields)
